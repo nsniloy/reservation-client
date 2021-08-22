5cdf4411b2b2feb126f785466a55c23ba78d788d
Nabid Salehin Niloy <niloy@evaly.com.bd>
2021-08-22 23:12:50 +0600

    initial setup

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar
+      fixed
+      app
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-btn
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title" />
+      <v-spacer />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      drawer: true,
+      fixed: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Dashboard',
+          to: '/admin/dashboard'
+        },
+        {
+          icon: 'mdi-home',
+          title: 'Vaccination Centres',
+          to: '/admin/centre'
+        }
+      ],
+      miniVariant: false,
+      left: true,
+      leftDrawer: true,
+      title: 'Homage'
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-row justify="center" align="center">
+    Dashboard
+  </v-row>
+</template>
+<script>
+export default {
+  layout:'customer'
+}
+</script>
